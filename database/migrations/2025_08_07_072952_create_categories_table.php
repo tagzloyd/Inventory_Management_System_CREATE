e0718f9b50b68a72829653b57c3c58473e606ed8
tagzloyd <loydtagaro1@gmail.com>
2025-08-11 08:50:11 +0800

    Update 2025_08_07_072952_create_categories_table.php

--- database/migrations/2025_08_07_072952_create_categories_table.php
+++ database/migrations/2025_08_07_072952_create_categories_table.php
@@ -1,22 +1,22 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-        });
-    }
+        public function up(): void
+        {
+            Schema::create('categories', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->timestamps();
+            });
+        }
 
-    public function down(): void
-    {
-        Schema::dropIfExists('categories');
-    }
-};
+        public function down(): void
+        {
+            Schema::dropIfExists('categories');
+        }
+    };
