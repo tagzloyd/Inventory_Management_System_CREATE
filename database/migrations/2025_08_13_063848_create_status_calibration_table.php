6bb1957b67abb7102b6cb99875ee7a5b96a1cf6b
tagzloyd <loydtagaro1@gmail.com>
2025-08-13 15:55:04 +0800

    Update 2025_08_13_063848_create_status_calibration_table.php

--- database/migrations/2025_08_13_063848_create_status_calibration_table.php
+++ database/migrations/2025_08_13_063848_create_status_calibration_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
          Schema::create('planned', function (Blueprint $table) {
                 $table->id();
-                $table->foreignId('cal_id')->constraint('calibration');
+                $table->foreignId('cal_id')->constrainted('calibration');
                 $table->string('jan')->nullable();
                 $table->string('feb')->nullable();
                 $table->string('mar')->nullable();
